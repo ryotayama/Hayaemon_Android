a45266b19d8171b792386a32c86974e1a5cfa408
ryotayama <ryota.yamauch@gmail.com>
2018-10-27 19:55:05 +0900

    close #142 コントロール画面のドラムロールについて表示がおかしくなってしまう事があったのを修正

--- app/src/main/java/com/edolfzoku/hayaemon2/PitchFragmentDialog.java
+++ app/src/main/java/com/edolfzoku/hayaemon2/PitchFragmentDialog.java
@@ -41,24 +41,30 @@ public class PitchFragmentDialog extends DialogFragment {
         int nIntPitch = (int)fPitch;
         int nDecimalPitch;
         if(fPitch >= 0.05) {
-            nDecimalPitch = (int) ((fPitch - (float) nIntPitch) * 10);
+            nDecimalPitch = (int) ((fPitch - (float) nIntPitch) * 10.0f + 0.05f);
         }
         else {
-            nDecimalPitch = (int) ((fPitch - (float) nIntPitch - 0.05) * 10);
+            nDecimalPitch = (int) ((fPitch - (float) nIntPitch) * 10.0f - 0.05f);
         }
         String strIntPitch;
         String strDecimalPitch;
         if(fPitch >= 0.05) {
-            strIntPitch = String.format("♯%d", nIntPitch);
+            if(nIntPitch < 10)
+                strIntPitch = String.format("♯%d ", nIntPitch);
+            else
+                strIntPitch = String.format("♯%d", nIntPitch);
             strDecimalPitch = String.format("%d", nDecimalPitch);
         }
         else {
-            strIntPitch = String.format("♭%d", nIntPitch * -1);
+            if(nIntPitch > -10)
+                strIntPitch = String.format("♭%d ", nIntPitch * -1);
+            else
+                strIntPitch = String.format("♭%d", nIntPitch * -1);
             strDecimalPitch = String.format("%d", nDecimalPitch * -1);
         }
 
         intNumberPicker = (NumberPicker)view.findViewById(R.id.intPitchPicker);
-        final String[] arInts = {"♯24", "♯23", "♯22", "♯21", "♯20", "♯19", "♯18", "♯17", "♯16", "♯15", "♯14", "♯13", "♯12", "♯11", "♯10", "♯9", "♯8", "♯7", "♯6", "♯5", "♯4", "♯3", "♯2", "♯1", "♯0", "♭0", "♭1", "♭2", "♭3", "♭4", "♭5", "♭6", "♭7", "♭8", "♭9", "♭10", "♭11", "♭12", "♭13", "♭14", "♭15", "♭16", "♭17", "♭18", "♭19", "♭20", "♭21", "♭22", "♭23", "♭24"};
+        final String[] arInts = {"♯24", "♯23", "♯22", "♯21", "♯20", "♯19", "♯18", "♯17", "♯16", "♯15", "♯14", "♯13", "♯12", "♯11", "♯10", "♯9 ", "♯8 ", "♯7 ", "♯6 ", "♯5 ", "♯4 ", "♯3 ", "♯2 ", "♯1 ", "♯0 ", "♭0 ", "♭1 ", "♭2 ", "♭3 ", "♭4 ", "♭5 ", "♭6 ", "♭7 ", "♭8 ", "♭9 ", "♭10", "♭11", "♭12", "♭13", "♭14", "♭15", "♭16", "♭17", "♭18", "♭19", "♭20", "♭21", "♭22", "♭23", "♭24"};
         intNumberPicker.setDisplayedValues(arInts);
         intNumberPicker.setMaxValue(49);
         intNumberPicker.setMinValue(0);
@@ -91,7 +97,7 @@ public class PitchFragmentDialog extends DialogFragment {
                 strInt = strInt.replace("♯", "");
                 strInt = strInt.replace("♭", "-");
                 String strDecimal = arDecimals[decimalNumberPicker.getValue()];
-                String strPitch = strInt + "." + strDecimal;
+                String strPitch = strInt.trim() + "." + strDecimal;
                 float fPitch = Float.parseFloat(strPitch);
 
                 MainActivity activity = (MainActivity)getActivity();
