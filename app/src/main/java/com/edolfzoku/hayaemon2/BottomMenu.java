8bfeab2965838793c87fc91d6c5fb330555b4520
ryotayama <ryota.yamauch@gmail.com>
2019-02-11 00:57:19 +0900

    close #192 下から表示されるボトムメニューについて、タップ時に背景色がグレーになるように対応

--- app/src/main/java/com/edolfzoku/hayaemon2/BottomMenu.java
+++ app/src/main/java/com/edolfzoku/hayaemon2/BottomMenu.java
@@ -8,6 +8,7 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -17,28 +18,40 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class BottomMenu extends BottomSheetDialog
-{
+public class BottomMenu extends BottomSheetDialog {
     LinearLayout linearLayout;
     ScrollView scroll;
     int nTag = 0;
 
-    public BottomMenu(@NonNull Context context)
-    {
+    public BottomMenu(@NonNull Context context) {
         super(context);
         linearLayout = new LinearLayout(context);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         scroll = new ScrollView(context);
         scroll.addView(linearLayout);
+        scroll.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    for(int i = 0; i < linearLayout.getChildCount(); i++) {
+                        View childView = linearLayout.getChildAt(i);
+                        if(childView.getHeight() != (int) (0.5 * getContext().getResources().getDisplayMetrics().density + 0.5))
+                            childView.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                    }
+                }
+                return false;
+            }
+        });
         setContentView(scroll);
-        BottomSheetBehavior behavior = BottomSheetBehavior.from((View)scroll.getParent());
+        BottomSheetBehavior behavior = BottomSheetBehavior.from((View) scroll.getParent());
         behavior.setPeekHeight(context.getResources().getDisplayMetrics().heightPixels);
         setDialogBorder(this);
+        setCancelable(false);
     }
 
     public void setDialogBorder(Dialog dialog) {
         FrameLayout bottomSheet = (FrameLayout) dialog.getWindow().findViewById(android.support.design.R.id.design_bottom_sheet);
-        setMargins(bottomSheet, (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5), 0, (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5), 0);
+        setMargins(bottomSheet, (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5), 0, (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5), 0);
     }
 
     private void setMargins(View view, int left, int top, int right, int bottom) {
@@ -49,43 +62,40 @@ public class BottomMenu extends BottomSheetDialog
         }
     }
 
-    public void setTitle(String strTitle)
-    {
+    public void setTitle(String strTitle) {
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        TextView textTitle = new TextView (getContext());
+        TextView textTitle = new TextView(getContext());
         textTitle.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);
         textTitle.setGravity(Gravity.CENTER);
         textTitle.setText(strTitle);
-        textTitle.setHeight((int)(48 *  getContext().getResources().getDisplayMetrics().density + 0.5));
+        textTitle.setHeight((int) (48 * getContext().getResources().getDisplayMetrics().density + 0.5));
         linearLayout.addView(textTitle, param);
 
         View viewSep = new View(getContext());
         viewSep.setBackgroundColor(Color.argb(255, 208, 208, 208));
         LinearLayout.LayoutParams paramViewSep = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramViewSep.topMargin = 0;
-        paramViewSep.bottomMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.leftMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.rightMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.height = (int)(0.5 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.bottomMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.leftMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.rightMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.height = (int) (0.5 * getContext().getResources().getDisplayMetrics().density + 0.5);
         linearLayout.addView(viewSep, paramViewSep);
     }
 
-    public void addSeparator()
-    {
+    public void addSeparator() {
         View viewSep = new View(getContext());
         viewSep.setBackgroundColor(Color.argb(255, 208, 208, 208));
         LinearLayout.LayoutParams paramViewSep = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        paramViewSep.topMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.bottomMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.leftMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.rightMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.height = (int)(0.5 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.topMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.bottomMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.leftMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.rightMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.height = (int) (0.5 * getContext().getResources().getDisplayMetrics().density + 0.5);
         linearLayout.addView(viewSep, paramViewSep);
     }
 
-    public void addMenu(String strText, int resID, android.view.View.OnClickListener listener)
-    {
-        RelativeLayout relativeLocal = new RelativeLayout(getContext());
+    public void addMenu(String strText, int resID, android.view.View.OnClickListener listener) {
+        final RelativeLayout relativeLocal = new RelativeLayout(getContext());
         relativeLocal.setTag(nTag);
         nTag += 1;
         ImageView imgLocal = new ImageView(getContext());
@@ -94,17 +104,27 @@ public class BottomMenu extends BottomSheetDialog
         RelativeLayout.LayoutParams paramImgLocal = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramImgLocal.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         paramImgLocal.addRule(RelativeLayout.CENTER_VERTICAL);
-        paramImgLocal.leftMargin = (int)(18 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramImgLocal.rightMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramImgLocal.leftMargin = (int) (18 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramImgLocal.rightMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
         relativeLocal.addView(imgLocal, paramImgLocal);
 
-        TextView textLocal = new TextView (getContext());
+        TextView textLocal = new TextView(getContext());
         textLocal.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
         textLocal.setGravity(Gravity.CENTER);
         textLocal.setText(strText);
         textLocal.setTextColor(Color.argb(255, 0, 0, 0));
-        textLocal.setHeight((int)(48 *  getContext().getResources().getDisplayMetrics().density + 0.5));
+        textLocal.setHeight((int) (48 * getContext().getResources().getDisplayMetrics().density + 0.5));
         relativeLocal.setOnClickListener(listener);
+        relativeLocal.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    relativeLocal.setBackgroundColor(Color.argb(255, 229, 229, 229));
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                    relativeLocal.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                return false;
+            }
+        });
         RelativeLayout.LayoutParams paramTextLocal = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramTextLocal.addRule(RelativeLayout.RIGHT_OF, imgLocal.getId());
         paramTextLocal.addRule(RelativeLayout.CENTER_VERTICAL);
@@ -113,26 +133,35 @@ public class BottomMenu extends BottomSheetDialog
         linearLayout.addView(relativeLocal, param);
     }
 
-    public void addDestructiveMenu(String strText, int resID, android.view.View.OnClickListener listener)
-    {
-        RelativeLayout relativeLocal = new RelativeLayout(getContext());
+    public void addDestructiveMenu(String strText, int resID, android.view.View.OnClickListener listener) {
+        final RelativeLayout relativeLocal = new RelativeLayout(getContext());
         ImageView imgLocal = new ImageView(getContext());
         imgLocal.setImageResource(resID);
         imgLocal.setId(1000);
         RelativeLayout.LayoutParams paramImgLocal = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramImgLocal.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         paramImgLocal.addRule(RelativeLayout.CENTER_VERTICAL);
-        paramImgLocal.leftMargin = (int)(18 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramImgLocal.rightMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramImgLocal.leftMargin = (int) (18 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramImgLocal.rightMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
         relativeLocal.addView(imgLocal, paramImgLocal);
 
-        TextView textLocal = new TextView (getContext());
+        TextView textLocal = new TextView(getContext());
         textLocal.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
         textLocal.setGravity(Gravity.CENTER);
         textLocal.setText(strText);
         textLocal.setTextColor(Color.argb(255, 255, 0, 0));
-        textLocal.setHeight((int)(48 *  getContext().getResources().getDisplayMetrics().density + 0.5));
+        textLocal.setHeight((int) (48 * getContext().getResources().getDisplayMetrics().density + 0.5));
         relativeLocal.setOnClickListener(listener);
+        relativeLocal.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    relativeLocal.setBackgroundColor(Color.argb(255, 229, 229, 229));
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                    relativeLocal.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                return false;
+            }
+        });
         RelativeLayout.LayoutParams paramTextLocal = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramTextLocal.addRule(RelativeLayout.RIGHT_OF, imgLocal.getId());
         paramTextLocal.addRule(RelativeLayout.CENTER_VERTICAL);
@@ -141,27 +170,36 @@ public class BottomMenu extends BottomSheetDialog
         linearLayout.addView(relativeLocal, param);
     }
 
-    public void setCancelMenu()
-    {
+    public void setCancelMenu() {
         View viewSep = new View(getContext());
         viewSep.setBackgroundColor(Color.argb(255, 208, 208, 208));
         LinearLayout.LayoutParams paramViewSep = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        paramViewSep.topMargin = (int)(8 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.topMargin = (int) (8 * getContext().getResources().getDisplayMetrics().density + 0.5);
         paramViewSep.bottomMargin = 0;
-        paramViewSep.leftMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.rightMargin = (int)(16 *  getContext().getResources().getDisplayMetrics().density + 0.5);
-        paramViewSep.height = (int)(0.5 *  getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.leftMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.rightMargin = (int) (16 * getContext().getResources().getDisplayMetrics().density + 0.5);
+        paramViewSep.height = (int) (0.5 * getContext().getResources().getDisplayMetrics().density + 0.5);
         linearLayout.addView(viewSep, paramViewSep);
 
-        TextView textCancel = new TextView (getContext());
+        final TextView textCancel = new TextView(getContext());
         textCancel.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
         textCancel.setGravity(Gravity.CENTER);
         textCancel.setText("キャンセル");
-        textCancel.setHeight((int)(48 *  getContext().getResources().getDisplayMetrics().density + 0.5));
+        textCancel.setHeight((int) (48 * getContext().getResources().getDisplayMetrics().density + 0.5));
         textCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dismiss();
+            }
+        });
+        textCancel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    textCancel.setBackgroundColor(Color.argb(255, 229, 229, 229));
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                    textCancel.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                return false;
             }
         });
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
