6c94a7c024db764fe25c35e0b78b1d5782cf25e4
ryotayama <ryota.yamauch@gmail.com>
2020-07-22 17:48:50 +0900

    リリースに向けてバージョン番号を変更

--- app/src/main/java/com/edolfzoku/hayaemon2/MainActivity.java
+++ app/src/main/java/com/edolfzoku/hayaemon2/MainActivity.java
@@ -782,7 +782,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             textViewBlog.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
 
             TextView textViewArticle = layout.findViewById(R.id.textViewArticle);
-            String strArticle = "<a href=\"http://hayaemon.jp/blog/archives/8028\">→該当記事へ</a>";
+            String strArticle = "<a href=\"http://hayaemon.jp/blog/archives/8036\">→該当記事へ</a>";
             CharSequence blogChar2 = Html.fromHtml(strArticle);
             textViewArticle.setText(blogChar2);
             textViewArticle.setMovementMethod(mMethod);
