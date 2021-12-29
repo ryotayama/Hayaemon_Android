a0bf8ddd43824f1525967e9c3538290fcbbf1579
ryotayama <ryota.yamauch@gmail.com>
2021-12-30 08:22:45 +0900

    #364 Constantsクラスに課金アイテムIDを追加

---
+++ app/src/main/java/com/edolfzoku/hayaemon2/Constants.java
@@ -0,0 +1,6 @@
+package com.edolfzoku.hayaemon2;
+
+class Constants {
+    // public static final String SKU_HIDEADS = "android.test.purchased"; // テスト用
+    public static final String SKU_HIDEADS = "hideads"; // 本番用
+}
